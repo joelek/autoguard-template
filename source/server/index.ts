de8b4d4a926c28805598fb55de2e2f50690252aa
Joel Ek <contact@joelek.se>
2024-12-08 16:13:10 +0100

    Update dependencies.

--- source/server/index.ts
+++ source/server/index.ts
@@ -1,4 +1,4 @@
-import * as autoguard from "@joelek/ts-autoguard/dist/lib-server";
+import * as autoguard from "@joelek/autoguard/dist/lib-server";
 import * as libhttp from "http";
 import * as libserver from "../shared/api/server";
 
